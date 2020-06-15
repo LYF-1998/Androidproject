950bef6459eb08fb4815b065a1ad3162c7ba5efb
kewudeqiutian <1159939145@qq.com>
2020-06-15 15:39:54 +0800

    6.6  9.42

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,75 +22,24 @@ public  class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button2);
+        ListView listView = findViewById(R.id.listview1);
+        String[] arr = {"qq", "ww", "ee"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, arr);
+        listView.setAdapter(adapter);
 
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-//                        String mu_username=u_username.getText().toString();
-//                        String mu_password=u_password.getText().toString();
-                        String sql1 = "INSERT INTO user VALUES('mu_username1','mu_password')";
-                        JDBCUtils.update(sql1);
-                        JDBCUtils.close();
-                    }
-                }).start();
-            }
-        });
-//        button.setOnClickListener(new View.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View v) {
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        String sql1 = "SELECT id, name, url FROM websites";
-//                        ResultSet resultSet = JDBCUtils.query(sql1);
-//                        try{
-//                            while(resultSet.next()){
-//                                int id  = resultSet.getInt("id");
-//                                String name = resultSet.getString("name");
-//                                String url = resultSet.getString("url");
-//                                String str = id + "******" + name + "*********" + url;
-//                                // 输出数据
-//                                //System.out.println(id + "******" + name + "*********" + url);
-//                                Log.i("eeeeee",str);
-//                            }
-//
-//                        } catch (SQLException e) {
-//                            e.printStackTrace();
-//                        }finally {
-//                            JDBCUtils.close();
-//                        }
-//                    }
-//                }).start();
-//            }
-//        });
-
+        String sql = "select address from user where username = 'test';";
+        String address = null;
+        ResultSet query = JDBCUtils.query(sql);
+        try {
+            query.next();
+            address = query.getString("address");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            JDBCUtils.close();
+        }
+        System.out.println(address);
     }
-//    public void test1()  {
-//        String sql1 = "SELECT id, name, url FROM websites";
-//        ResultSet resultSet = JDBCUtils.query(sql1);
-//        try{
-//            while(resultSet.next()){
-//                int id  = resultSet.getInt("id");
-//                String name = resultSet.getString("name");
-//                String url = resultSet.getString("url");
-//                String str = id + "******" + name + "*********" + url;
-//                // 输出数据
-//                //System.out.println(id + "******" + name + "*********" + url);
-//                Log.i("eeeeee",str);
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }finally {
-//            JDBCUtils.close();
-//        }
-//
-//    }
 
 
 }
